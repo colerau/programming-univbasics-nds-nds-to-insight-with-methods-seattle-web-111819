6929060c677aeb7ba1efda72e5d1d3e5f7a808a5
Cole Rau <colevrau@gmail.com>
2019-11-13 08:34:36 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -11,7 +11,7 @@ require "pp"
 def directors_totals(nds)
   nds.each do |name_movies|
     director_name = name_movies[:name]
-    hash.store(director_name, gross)
+    hash.store(director_name, gross_for_director(nds))
 
 end
 
