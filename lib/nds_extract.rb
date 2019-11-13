3ba081e69238567f8ab0150707f0ce9c7b97a3c4
Cole Rau <colevrau@gmail.com>
2019-11-13 08:15:48 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -21,6 +21,7 @@ end
 # using director_data as input
 def gross_for_director(director_data)
     pp director_data
+    gross = 0 
     director_data.each do |name_movies|
       gross = 0
       name_movies[:movies].each do |movie|
