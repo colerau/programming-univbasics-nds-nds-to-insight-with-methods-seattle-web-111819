4c3fb8076e443a2e3ac07b333a4c424241731188
Cole Rau <colevrau@gmail.com>
2019-11-13 08:28:25 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -11,7 +11,7 @@ require "pp"
 def directors_totals(nds)
   nds.each do |name_movies|
     gross = 0 
-    director_name = name_movies{:name
+    director_name = name_movies[:name]
     name_movies[:movies].each do |movie|
       director_name = 
       gross += movie[:worldwide_gross] 
