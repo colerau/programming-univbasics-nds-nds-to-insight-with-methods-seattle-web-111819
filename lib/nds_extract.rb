430aafc43724f7f765bfc603d4289ce85b6f1f6d
Cole Rau <colevrau@gmail.com>
2019-11-13 08:25:49 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -21,8 +21,6 @@ end
 # Find a way to accumulate the :worldwide_grosses and return that Integer
 # using director_data as input
 def gross_for_director(director_data)
-    nds.each do |name_movies|
-    # pp director_data
     gross = 0 
     director_data.each do |name_movies|
       name_movies[:movies].each do |movie|
